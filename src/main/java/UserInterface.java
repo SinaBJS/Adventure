55ec2c25fb9e59ddf73352ed3b83f02de3463590
SinaBJS <sija0001@stud.kea.dk>
2023-09-25 15:16:06 +0200

    First commit. Main, adventure og room klasser oprettet

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class UserInterface {
+
+
+    public void start() {
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Type look to look around ");
+        System.out.println("Type help for instructions");
+        System.out.println("Type exit to shutdown the program");
+        while (true) {
+            String userchoice = keyboard.nextLine();
+
+
+            switch (userchoice.toLowerCase()){
+                case "look":
+                    System.out.println();
+                System.out.println("Looking around");
+                break;
+                case "help":
+                System.out.println("Type a direction (North,South,West,East) to go in that direction");
+                break;
+                case "exit":
+                System.out.println("Shutting down the program");
+                break;
+            }
+
+
+        }
+    }
 }
