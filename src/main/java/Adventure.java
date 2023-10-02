b80b8087c853e13658be5d8c8f238006a01f63a2
SinaBJS <sija0001@stud.kea.dk>
2023-10-02 12:48:07 +0200

    help command expanded

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -15,52 +15,8 @@ public class Adventure {
     }
 
 
-
-    /*public void goEast() {
-        if (currentRoom.east == null) {
-            System.out.println("I can't go this way.");
-        } else {
-            System.out.println("Going east...");
-            currentRoom = currentRoom.east;
-        }
-    }
-
-    public void goNorth() {
-        if (currentRoom.north == null) {
-            System.out.println("I can't go this way.");
-        } else {
-            System.out.println("Going north...");
-            currentRoom = currentRoom.north;
-        }
-
-    }
-
-    public void goWest() {
-        if (currentRoom.west == null) {
-            System.out.println("I cant go this way.");
-        } else {
-            System.out.println("Going west...");
-            currentRoom = currentRoom.west;
-        }
-
-    }
-
-    public void goSouth() {
-        if (currentRoom.south == null) {
-            System.out.println("I can't go this way.");
-        } else {
-            System.out.println("Going south...");
-            currentRoom = currentRoom.south;
-        }
-    }
-    public Room getCurrentRoom(){
-        return currentRoom;
-    }*/
-
    /* public void xyzzy(){
         currentRoom = room1;
-
-
         System.out.println("You just teleported");
     }
 */
--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -10,14 +10,19 @@ public class UserInterface {
         System.out.println("Type look to look around ");
         System.out.println("Type help for instructions");
         System.out.println("Type exit to shutdown the program");
-        Player player =adventure.getPlayer();
+        Player player = adventure.getPlayer();
         while (true) {
             String userchoice = keyboard.nextLine();
 
             switch (userchoice.trim().toLowerCase()) {
-                case "look" -> player.lookAround();
-                case "help" ->
-                        System.out.println("Type a direction (for example 'go west', 'west' or 'w') to go in that direction");
+                case "look around", "look", "l" -> player.lookAround();
+                case "help" -> {
+                    System.out.println("Type a direction (for example 'go west', 'west' or 'w') to go in that direction");
+                    System.out.println("Look around you by typing 'look around', 'look' or 'l'");
+                    System.out.println("Turn on the light by typing 'turn on' or 'light on");
+                    System.out.println("Turn off the light by typing 'turn off' or 'light off'");
+                }
+
                 case "go east", "east", "e" -> {
                     player.goEast();
                     player.lookAround();
@@ -32,7 +37,7 @@ public class UserInterface {
                 }
                 case "go south", "south", "s" -> {
                     player.goSouth();
-                   player.lookAround();
+                    player.lookAround();
                 }
                 case "turn on", "light on" -> {
 
