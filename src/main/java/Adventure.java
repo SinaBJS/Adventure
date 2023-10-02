a899fd6333e6fcffee6a11fe9fc2e391b67c988e
LasseThomsenSkole <lath0003@stud.kea.dk>
2023-10-02 13:26:52 +0200

    comments

--- src/main/java/Adventure.java
+++ src/main/java/Adventure.java
@@ -7,8 +7,7 @@ public class Adventure {
     }
 
     private Player player;
-    private Room room;
-    //public ArrayList<Item> itemsRoom = room.getItems();
+
     public Adventure() {
         Map map = new Map();
         map.buildMap();
@@ -18,53 +17,4 @@ public class Adventure {
     }
 
 
-
-    /*public void goEast() {
-        if (currentRoom.east == null) {
-            System.out.println("I can't go this way.");
-        } else {
-            System.out.println("Going east...");
-            currentRoom = currentRoom.east;
-        }
-    }
-
-    public void goNorth() {
-        if (currentRoom.north == null) {
-            System.out.println("I can't go this way.");
-        } else {
-            System.out.println("Going north...");
-            currentRoom = currentRoom.north;
-        }
-
-    }
-
-    public void goWest() {
-        if (currentRoom.west == null) {
-            System.out.println("I cant go this way.");
-        } else {
-            System.out.println("Going west...");
-            currentRoom = currentRoom.west;
-        }
-
-    }
-
-    public void goSouth() {
-        if (currentRoom.south == null) {
-            System.out.println("I can't go this way.");
-        } else {
-            System.out.println("Going south...");
-            currentRoom = currentRoom.south;
-        }
-    }
-    public Room getCurrentRoom(){
-        return currentRoom;
-    }*/
-
-   /* public void xyzzy(){
-        currentRoom = room1;
-
-
-        System.out.println("You just teleported");
-    }
-*/
 }
