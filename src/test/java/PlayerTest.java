2496f3fba353d3f3f73795efccf82bc1b2acbec3
LasseThomsenSkole <lath0003@stud.kea.dk>
2023-10-06 09:28:42 +0200

    small changes

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,5 +1,8 @@
+import Items.Food;
+import Items.Item;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,7 +15,7 @@ class PlayerTest {
     @BeforeEach
     void setUp(){
         room1 = new Room("room1", "room number 1", true);
-        player = new Player(room1, 100);
+        player = new Player(room1, 100,null);
         item = new Item("item", "itemitem");
         food = new Food("banan", "itemitem", 20);
         player.inventory.add(item);
